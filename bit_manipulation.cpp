a9a4185305a4b6400f2c8e424a26c833fdf387de
Chitrang <chitrang6@gmail.com>
2017-04-22 16:06:27 -0700

    Added the function to play with rightmost set bit

--- bit_manipulation.cpp
+++ bit_manipulation.cpp
@@ -8,6 +8,7 @@ This file contains important bit manipulation problems in C++
 
 #include <iostream>
 #include <bitset>
+#include <math.h>
 
 using namespace std;
 
@@ -48,6 +49,38 @@ int ToggleKthBit(int num, int pos)
 
 
 
+// Problems based on unsetting right most set bit in the number.
+
+
+int unsetRightMostSetBit(int num)
+{
+	return (num & (num - 1));
+}
+
+
+// Find the given number is power of 2 or not
+
+int isPowerofTwo(int num)
+{
+	return (num & (num - 1));
+}
+
+
+
+// Finding the position the right most set bit
+
+int positionOfRightMostSetBit(int num)
+{
+	if(num & (num - 1))
+		return (log2(num) + 1);
+
+	else
+	{
+		return (log2(num & -num) + 1);
+	}
+}
+
+
 
 
 
@@ -72,4 +105,18 @@ int main()
 		cout<<"15th Bit is not set"<<endl;
 
 	cout<< "Toggle the ninth bit in number var: "<< bitset<16>(ToggleKthBit(var, 9)) <<endl;
+
+
+	cout << "Unset the right most set bit: "<<bitset<16>(unsetRightMostSetBit(var))<<endl;
+	
+	int num = 16;
+
+	if(!isPowerofTwo(num))
+		cout << "The number is power of two."<<endl;
+
+	else
+		cout<<"Not power of two"<<endl;
+
+
+	cout<<"Position of the most right set bit is:"<< positionOfRightMostSetBit(num) << endl;
 }
